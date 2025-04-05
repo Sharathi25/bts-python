ee77235e18a9cfe13fbee23d972ef637009d7091
Sharathi25 <sharathir25@gmail.com>
2025-04-05 18:00:21 +0530

    set updated

--- set.py
+++ set.py
@@ -2,3 +2,24 @@
 
 new_set = {1,2,2,3,2,7,5}
 print(new_set)
+
+
+# add values in set 
+
+new_set.add(10)
+print(new_set)
+
+#  add the values in a to b
+
+a = {1,2,3,}
+b = {4,2,7}
+
+a.update(b)
+print(a)
+b.update(a)
+print(b)
+
+print(a.union(b))
+print(a.intersection_update(b))
+print(a.intersection(b))
+print(a.isdisjoint(b))
